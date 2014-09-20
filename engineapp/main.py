5fa3360abbabbf13ca27ab8c62c691d210cf2ab3
tpcstld <jerryjiang1128@gmail.com>
2014-09-20 10:48:49 -0400

    Querying by the other things now

--- engineapp/main.py
+++ engineapp/main.py
@@ -7,11 +7,15 @@ from google.appengine.ext import ndb
 import json
 import geopy
 import geopy.distance
+import time
+from datetime import datetime
 app = Flask(__name__)
 
 # Note: We don't need to call run() since our application is embedded within
 # the App Engine WSGI application server.
-    
+
+def get_date(date_string):
+    return datetime.fromtimestamp(time.mktime(time.strptime(date_string, "%Y-%m-%d")))    
 
 @app.route('/api/sublets/<int:sublet_id>', methods=['GET', 'PUT', 'DELETE'])
 def api_sublet_with_id(sublet_id):
@@ -38,6 +42,8 @@ def api_sublets():
         offset = int(request.args.get("offset", 0))
         minimum_price = float(request.args.get("minimum_price", 0))
         maximum_price = float(request.args.get("maximum_price", 1000000))
+        start_date = get_date(request.args.get("start_date"))
+        end_date = get_date(request.args.get("end_date"))
         #geosearch
         center = geopy.Point(float(request.args.get("latitude")), float(request.args.get("longitude")))
         radius = float(request.args.get("radius"))
@@ -53,17 +59,18 @@ def api_sublets():
                                  SubletEntity.price <= maximum_price,
                                  *[SubletEntity.tags == tag for tag in tags])
         sublets = qry.fetch()
-        more = len(sublets) > offset + limit
         #should implement google's searches later
         infos = []
-        for s in sublets[offset:offset + limit]:
+        for s in sublets:
             distance = geopy.distance.distance(geopy.Point(s.location.lat, s.location.lon), center).kilometers
-            if distance <= radius:
+            if distance <= radius and s.rooms_available > 0 and s.start_date <= start_date and s.end_date >= end_date:
                 info = s.Get()
                 info["distance"] = distance
                 infos.append(info)
+
+        more = len(infos) > offset + limit
         #infos = [s.Get() for s in sublets[offset:offset + limit] if geopy.distance.distance(geopy.Point(s.location.lat, s.location.lon), center) <= radius]
-        return Response(json.dumps({"limit": limit, "offset": offset, "more": more, "sublets": infos}), mimetype="application/json")
+        return Response(json.dumps({"limit": limit, "offset": offset, "more": more, "sublets": infos[offset:offset + limit]}), mimetype="application/json")
 
 @app.route('/api/users', methods=['POST'])
 def api_users():
